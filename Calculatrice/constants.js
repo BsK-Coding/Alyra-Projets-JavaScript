7c75ec531fb02517191cd4a6f3085b5ad26c35c7
BsK-Coding <blackshark.blog@gmail.com>
2021-08-09 12:42:09 +0200

    ok

---
+++ Calculatrice/constants.js
@@ -0,0 +1,20 @@
+const ADD_OPERATOR = '+'
+const SUB_OPERATOR = '-'
+const MUL_OPERATOR = '*'
+const DIV_OPERATOR = '/'
+const MODULO_OPERATOR = '%'
+
+exports.ADD_OP = ADD_OPERATOR
+exports.SUB_OP = SUB_OPERATOR
+exports.MUL_OP = MUL_OPERATOR
+exports.DIV_OP = DIV_OPERATOR
+exports.MOD_OP = MODULO_OPERATOR
+
+/* Autres méthodes */
+/*
+exports.ADD_OP = '+'
+exports.SUB_OP = '-'
+exports.MUL_OP = '*'
+exports.DIV_OP = '/'
+exports.MOD_OP = '%'
+*/
---
+++ Calculatrice/operations.js
@@ -0,0 +1,49 @@
+const add = (nb1, nb2) => {
+  return nb1 + nb2
+}
+
+const sub = (nb1, nb2) => {
+  return nb1 - nb2
+}
+
+const mul = (nb1, nb2) => {
+  return nb1 * nb2
+}
+
+const div = (nb1, nb2) => {
+  return nb1 / nb2
+}
+
+const modulo = (nb1, nb2) => {
+  return nb1 % nb2
+}
+
+exports.add = add
+exports.sub = sub
+exports.mul = mul
+exports.div = div
+exports.modulo = modulo
+
+/* Autres méthodes */
+/*
+// operations.js
+exports.add = (nb1, nb2) => {
+  return nb1 + nb2
+}
+
+exports.sub = (nb1, nb2) => {
+  return nb1 - nb2
+}
+
+exports.mul = (nb1, nb2) => {
+  return nb1 * nb2
+}
+
+exports.div = (nb1, nb2) => {
+  return nb1 / nb2
+}
+
+exports.modulo = (nb1, nb2) => {
+  return nb1 % nb2
+}
+*/
